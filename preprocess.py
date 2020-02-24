0610db39b78722cad0dc0edd6be18d016f297294
mike-koh <52910468+mike-koh@users.noreply.github.com>
2020-02-24 17:24:15 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -7,9 +7,9 @@ TEMP_DATA_BUCKET = get_temp_data_bucket()
 PREPROCESSED_DATA = TEMP_DATA_BUCKET + os.getenv("PREPROCESSED_DATA")
 
 def main():
-    """Preprocess data"""
-    print("\tPreprocessing")
-    subscribers = pd.read_csv(RAW_SUBSCRIBERS_DATA)
+    	"""Preprocess data"""
+	print("\tPreprocessing")
+	subscribers = pd.read_csv(RAW_SUBSCRIBERS_DATA)
 	subscribers = subscribers.drop(['y_no'],axis=1)
 	subscribers.to_csv(PREPROCESSED_DATA,index=false)
 
