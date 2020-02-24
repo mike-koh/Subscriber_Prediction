482c4be4312cfe8176620de2dfea805ba598056d
mike-koh <52910468+mike-koh@users.noreply.github.com>
2020-02-24 17:32:18 +0800

    tabs and spaces in indentation

--- preprocess.py
+++ preprocess.py
@@ -7,11 +7,11 @@ TEMP_DATA_BUCKET = get_temp_data_bucket()
 PREPROCESSED_DATA = TEMP_DATA_BUCKET + os.getenv("PREPROCESSED_DATA")
 
 def main():
-    	"""Preprocess data"""
-	print("\tPreprocessing")
-	subscribers = pd.read_csv(RAW_SUBSCRIBERS_DATA)
-	subscribers = subscribers.drop(['y_no'],axis=1)
-	subscribers.to_csv(PREPROCESSED_DATA,index=false)
+    """Preprocess data"""
+    print("\tPreprocessing")
+    subscribers = pd.read_csv(RAW_SUBSCRIBERS_DATA)
+    subscribers = subscribers.drop(['y_no'],axis=1)
+    subscribers.to_csv(PREPROCESSED_DATA,index=false)
 
 if __name__ == "__main__":
     main()
